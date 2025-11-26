015b99213853098408665439a443ad10c5f9cbc0
Nisa Jamalia Nurhayati <nisajamalia99@gmail.com>
2025-11-26 23:11:44 +0700

    hello

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\ProdukController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/produk/list', [ProdukController::class, 'list']);
+Route::get('/produk/list', [ProdukController::class, 'list']);
